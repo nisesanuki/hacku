baacbb8ae09fe92febb5cde2c24cf4535ba3613a
Seiya Komatsu <a18506@sr.kagawa-nct.ac.jp>
2019-08-23 16:36:44 +0900

    APIリストの追加

--- api/webapi.py
+++ api/webapi.py
@@ -2,27 +2,12 @@ from flask import Flask, jsonify, abort, make_response, render_template, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import MySQLdb
+import qrcode
 
 api = Flask(__name__)
 CORS(api)  # CORS有効化
 
-@api.route('/get', methods=['GET'])  # Getだけ受け付ける
-def get():  # 関数名は重複していなければなんでもよい
-    result = ""
-    # ローカルのファイルを全部読み込んで返すだけ
-    with open("./datafile", mode='r') as f:
-        result += f.read()
-    return result
-
-@api.route('/post', methods=['POST'])  # Postだけ受け付ける
-def post():
-    result = request.form["param"]  # Postで送ったときのパラメータの名前を指定する
-    # パラメータをローカルのファイルに書き込むだけ
-    with open("./datafile", mode='a') as f:
-        f.write(result + "\n")
-    return make_response(result)
-
-
+# ログイン API
 @api.route('/login', methods=['POST'])
 def login():
     email = request.form['email']
@@ -50,8 +35,77 @@ def login():
         return "あはは"
     else:
         return "ふぇぇ"
+        
 
+# ユーザ登録 API
+@api.route('/regist', methods=['POST'])
+def regist():
+    email = request.form['email']
+    password = ['password']
 
-# 4000番ポートでWebサーバを起動する
+    # SQL 実行
+    connection = MySQLdb.connect(
+        host = 'localhost',
+        user = 'hacku',
+        password = 'hacku',
+        db = 'hacku')
+    cursor = connection.cursor()
+
+    cursor.execute("INSERT INTO hacku.users VALUE")
+
+    connection.commit()
+    connection.close()
+
+    # 実行結果処理
+    
+
+# QR 登録処理
+@api.route('/genqr', methods=['POST'])
+def genqr():
+    email = request.form['email']
+    qrname = request.form['qrname']
+    
+    # SQL 実行
+    connection = MySQLdb.connect(
+        host = 'localhost',
+        user = 'hacku',
+        password = 'hacku',
+        db = 'hacku')
+    cursor = coonection.cursor()
+
+    cursor.execute("INSERT INTO hacku.user VALUE")
+
+    connection.commit()
+    connection.close()
+
+    # 実行結果処理
+
+# QR 一覧表示
+@api.route('/listqr', methods=['POST'])
+def listqr():
+    email = request.form['email']
+    
+    # SQL 実行
+    connection = MySQLdb.connect(
+        host = 'localhost',
+        user = 'hacku',
+        password = 'hacku',
+        db = 'hacku')
+    cursor = coonection.cursor()
+
+    cursor.execute("INSERT INTO hacku.user VALUE")
+
+    connection.commit()
+    connection.close()
+
+    return cursor
+
+# 登録成功時の処理
+@api.route('/regist_success', methods=['POST'])
+def regist_success():
+    pass
+    # メール送信等
+
+# 37564番ポートでWebサーバを起動する
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=37564, debug=True)
--- js/main.js
+++ js/main.js
@@ -21,6 +21,13 @@ function login()
     $.post("http://localhost:37564/login",
            {email:email, password:pass},
            function(data){alert(data)});
+    
+    // ログイン成功時処理
+    $.session.set(email, email);
+    window.location.href = "mypage.html"
+
+    // ログイン失敗時処理
+    
 }
 
 function user_registration()
@@ -46,6 +53,10 @@ function user_registration()
     $.post("http://localhost:37564/regist",
            {email:email, password:pass},
            function(data){alert(data)});
-}
 
+    // 登録成功時処理
+
+
+    // 登録失敗時処理
+}
 
